36843d85efbcda6f9a146685cfe8b6d992cf7d8a
parvathikarthika <87369341+parvathikarthika@users.noreply.github.com>
2021-10-12 08:58:11 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,9 @@
+class Box extends baseClass{
+  constructor(x, y, width, height) {
+    super(x,y,width,height) ;
+    this.image=loadImage("sprites/wood1.png");
+  }
+  display(){
+  super.display();
+  } 
+};
---
+++ Pig.js
@@ -0,0 +1,11 @@
+class Pig extends baseClass{
+    constructor(x, y) {
+      super(x,y,50,50) ;
+      this.image=loadImage("sprites/enemy.png");
+      
+    }
+    display(){
+      super.display();
+  }
+  };
+  
